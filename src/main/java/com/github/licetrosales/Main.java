bb60aeee891c7755c99e7b5bdcb437ce7e3f93c7
licetullmann <licetrosales@gmail.com>
2022-12-02 22:24:22 +0100

    Add Main and MainTest Classe

--- src/main/java/com/github/licetrosales/Main.java
+++ src/main/java/com/github/licetrosales/Main.java
@@ -1,2 +1,7 @@
-package com.github.licetrosales;public class Main {
+package com.github.licetrosales;
+
+public class Main {
+    public static int sum(int a, int b) {
+        return a+b;
+    }
 }
--- src/test/java/com/github/licetrosales/MainTest.java
+++ src/test/java/com/github/licetrosales/MainTest.java
@@ -1,4 +1,25 @@
+package com.github.licetrosales;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+
+    @Test
+    void when_1_add_1_then_return_2(){
+        //GIVEN
+        int a = 1;
+        int b = 1;
+
+        //WHEN
+        int result = Main.sum(a, b);
+
+        //THEN
+        int expected = 2;
+
+        assertEquals(expected, result);
+    }
+
+
 }
